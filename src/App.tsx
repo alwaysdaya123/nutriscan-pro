cbf9396d0340e0937a44b660bec4e7ca769230f7
daya123 <alwaysdaya123@gmail.com>
2026-03-19 11:27:47 +0530

    my local changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 import { AdminRoute } from "@/components/admin/AdminRoute";
@@ -24,6 +25,7 @@ import DietHistoryPage from "./pages/DietHistoryPage";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 import TermsPage from "./pages/TermsPage";
 import NotFound from "./pages/NotFound";
+
 
 // Admin Pages
 import AdminDashboardPage from "./pages/admin/AdminDashboardPage";
@@ -49,7 +51,7 @@ const App = () => (
           <AccessibilityProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <HashRouter>
               <AuthProvider>
                 <Routes>
                   <Route path="/" element={<LandingPage />} />
@@ -161,7 +163,7 @@ const App = () => (
                   <Route path="*" element={<NotFound />} />
                 </Routes>
               </AuthProvider>
-            </BrowserRouter>
+            </HashRouter>
           </AccessibilityProvider>
         </TooltipProvider>
       </QueryClientProvider>
